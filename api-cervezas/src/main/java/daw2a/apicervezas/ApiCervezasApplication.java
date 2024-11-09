c5cae373a077f9c89174460cfafeaaabe1e9e439
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-09 11:59:40 +0100

    carpeta proyecto

---
+++ api-cervezas/src/main/java/daw2a/apicervezas/ApiCervezasApplication.java
@@ -0,0 +1,13 @@
+package daw2a.apicervezas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCervezasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCervezasApplication.class, args);
+	}
+
+}
